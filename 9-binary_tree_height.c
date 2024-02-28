3a404703773ee7efe900eec7c2af2d010d6d14e0
CHIGODI <chiegody254@gmail.com>
2024-02-28 07:34:57 +0300

    Height of tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,10 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left, right;
+        size_t height_l;
+        size_t height_r;
 
-	if (tree == NULL)
-		return (0);
-
-	right = binary_tree_height(tree->right);
-	left = binary_tree_height(tree->left);
-
-	return ((right > left ? right : left) + 1);
+        height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+        height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+        return (height_l > height_r ? height_l : height_r);
 }
