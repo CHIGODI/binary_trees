1e11964916a0faf90a6149ce04a0692bb7f3dee2
CHIGODI <chiegody254@gmail.com>
2024-02-29 07:56:26 +0300

    Use diff find height code

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,24 +1,32 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 /**
- * binary_tree_balance -  a function that measures the balance
- *                        factor of a binary tree
- * @tree: a pointer to the root node of the tree to measure the height.
- *
- * Return: balance factor
- *
+ * find_height - determine the height of a binary tree
+ * @tree: pointer to root node
+ * Return: height of the binary tree
+ */
+size_t find_height(const binary_tree_t *tree)
+{
+	size_t height_l, height_r;
+
+	if (tree == NULL)
+		return (0);
+
+	height_l = find_height(tree->left);
+	height_r = find_height(tree->right);
+
+	return ((height_l > height_r ? height_l : height_r) + 1);
+}
+
+/**
+ * binary_tree_balance - checks the distribution of nodes
+ * between left and righ branches
+ * @tree: pointer to the node of interest
+ * Return: the balance (left nodes - right nodes)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int height_l = 0, height_r = 0, balance_factor;
+	if (tree == NULL)
+		return (0);
 
-	if (tree && tree->left)
-		height_l = 1 + binary_tree_balance(tree->left);
-
-	if (tree && tree->right)
-		height_r = 1 + binary_tree_balance(tree->right);
-
-	balance_factor = height_l - height_r;
-
-	return (balance_factor);
+	return (find_height(tree->left) - find_height(tree->right));
 }
